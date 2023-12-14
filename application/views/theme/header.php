47fa83ad7e5aa93f15b49355087174badbc6f4c6
Yoyowaimo <rio22ti@mahasiswa.pcr.ac.id>
2023-12-14 23:34:17 +0700

    commit 1

---
+++ application/views/theme/header.php
@@ -0,0 +1,19 @@
+
+            <div class="header-top_area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="header_top_wrap d-flex justify-content-between align-items-center">
+                                <div class="text_wrap">
+                                    <p><span>Call : +62882 7900 9454</span> | <span>Email : rio22ti@mahasiswa.pcr.ac.id</span></p>
+                                </div>
+                                <div class="text_wrap">
+                                    <p><a href="<?php echo site_url('admin/login') ?>">Login</a> <a href="#">Admin</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        
